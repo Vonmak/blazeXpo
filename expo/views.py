ff173590f7946524ce0d8024fc1e71bbb12a5d66
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-10 15:15:14 +0300

    created a login function.

--- expo/views.py
+++ expo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect, get_object_or_404
 from django.http import HttpResponse
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def register(request):
@@ -13,5 +14,23 @@ def register(request):
             user.profile.email = form.cleaned_data.get('email')
             user.save()
             form.save()
-        return HttpResponse('Registered Successfully')
+        return redirect(login_user)
     return render(request, 'auth/register.html',locals())
+
+def login_user(request):
+    form=LoginForm()
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            usern=form.cleaned_data['email']
+            passw=form.cleaned_data['password']
+            user=authenticate(request,username=usern,password=passw)
+            if user is not None:
+                login(request,user)
+                return redirect(register)
+            else:
+                return HttpResponse('Such a user does not exist')
+        else:
+            return HttpResponse("Form is not Valid")
+    
+    return render(request,'auth/login.html',locals())
