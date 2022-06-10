650131fa54d20aa9cfa68bcdf7629694bbf0e4d9
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-10 15:15:42 +0300

    added a login urls.

--- expo/urls.py
+++ expo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns=[
-    path('register/',views.register)
+    path('register/',views.register),
+    path('login/',views.login_user),
  
 ]
