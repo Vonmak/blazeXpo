80744e00f40d018abb2f8a9c63fd3bee44e47f76
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-13 18:24:28 +0300

    witched the form to the index.

--- expo/views.py
+++ expo/views.py
@@ -4,11 +4,31 @@ from .forms import *
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import *
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+from rest_framework import status
+from rest_framework import viewsets
 
 # Create your views here.
 def index(request):
+    project=Project.objects.latest('date')
     projects = Project.objects.all().order_by('-date')
     profiles= Profile.objects.all()
+    current_user = request.user
+    if request.method == 'POST':   
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid(): 
+            post = form.save(commit=False) 
+            post.user=request.user
+            post.profile=request.user.profile
+            post.save() 
+            return redirect(index) 
+              
+            # return HttpResponseRedirect(request.path_info)  
+    else:  
+        form = ProjectForm()  
     return render(request,'index.html',locals())
 
 
@@ -68,23 +88,6 @@ def update_profile(request):
     return render(request, 'profile/update.html', locals())
 
 
-def project_add(request):  
-    current_user = request.user
-    if request.method == 'POST':   
-        form = ProjectForm(request.POST, request.FILES)
-        if form.is_valid(): 
-            post = form.save(commit=False) 
-            post.user=request.user
-            post.profile=request.user.profile
-            post.save() 
-            return redirect(index) 
-              
-            # return HttpResponseRedirect(request.path_info)  
-    else:  
-        form = ProjectForm()  
-  
-    return render(request, 'project.html', locals())  
-
 def search(request):
     profiles = User.objects.all()
 
@@ -99,14 +102,61 @@ def search(request):
     return redirect(index)
 
 def search_results(request):
-    # title=Category.name
     if 'project_name' in request.GET and request.GET["project_name"]:
         search_term = request.GET.get("project_name")
         searched_projects = Project.search_by_project_name(search_term)
         message = f"{search_term}"
-        # print(searched_images)
+
         return render(request, 'search.html',locals())
 
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
+
+
+# class UserList(APIView):
+#     def get(self, request, format=None):
+#         projects = User.objects.all()
+#         serializers = UserSerializer(projects, many=True)
+#         return Response(serializers.data)
+    
+    
+# def project(request, project):
+#     project = Project.objects.get(project_name=project)
+#     ratings = Rating.objects.filter(user=request.user, project=project).first()
+#     rating_status = None
+#     if ratings is None:
+#         rating_status = False
+#     else:
+#         rating_status = True
+#     if request.method == 'POST':
+#         form = RatingsForm(request.POST)
+#         if form.is_valid():
+#             rate = form.save(commit=False)
+#             rate.user = request.user
+#             rate.project = project
+#             rate.save()
+#             project_ratings = Rating.objects.filter(project=project)
+
+#             design_ratings = [d.design for d in project_ratings]
+#             design_average = sum(design_ratings) / len(design_ratings)
+
+#             usability_ratings = [us.usability for us in project_ratings]
+#             usability_average = sum(usability_ratings) / len(usability_ratings)
+
+#             content_ratings = [content.content for content in project_ratings]
+#             content_average = sum(content_ratings) / len(content_ratings)
+
+#             score = (design_average + usability_average + content_average) / 3
+#             # print(score)
+#             rate.design_average = round(design_average, 2)
+#             rate.usability_average = round(usability_average, 2)
+#             rate.content_average = round(content_average, 2)
+#             rate.score = round(score, 2)
+#             rate.save()
+#             return HttpResponseRedirect(request.path_info)
+#     else:
+#         form = RatingsForm()
+  
+#     return render(request, 'pro.html', locals())
+
