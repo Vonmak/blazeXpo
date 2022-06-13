f570742472d046764b18519b1c325a494afa99a0
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-13 18:27:10 +0300

    updated the urls.

--- expo/urls.py
+++ expo/urls.py
@@ -8,7 +8,11 @@ urlpatterns=[
     path('logout',logout_user),
     re_path(r'^profile/(\d+)',profile, name='profile'),
     path('profile/',update_profile,name ='newprofile'),
-    path('project/', project_add, name = "project"),
+    # path('', project_add, name = "project"),
     path('search/',search, name='search'),
     path('search/',search_results, name='search'),
+    # path('api/projects/', ProjectList.as_view()),
+    # path('api/profiles/', ProfileList.as_view()),
+    path('api/users/', UserList.as_view()),
+    path('project/<project>', project, name='project'),
 ]
